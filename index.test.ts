54264b784dc6553ac2e4a1ddc9f0af2a3898b7a2
sillycoon <ahtunget@gmail.com>
2023-04-14 03:30:23 +0900

    remove title if there is no such symbol

--- index.test.ts
+++ index.test.ts
@@ -4,3 +4,7 @@ import { getSymbolByTitle } from './index';
 test('get correct unicode symbol', () => {
   expect(getSymbolByTitle('GEAR')).toBe('\u2699');
 });
+
+test('returns title if there is no such symbol', () => {
+  expect(getSymbolByTitle('KEK' as any)).toBe('KEK');
+});
--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import unicode from './unicode.json' assert { type: 'json' };
 
 export type Unicode = typeof unicode;
-export const getSymbolByTitle = (title: keyof Unicode) =>
-  hexToUnicode(unicode[title]);
+/**
+ * @returns Unicode representation of the title or the title itself
+ * if there is no such symbol
+ */
+export const getSymbolByTitle = (title: keyof Unicode): string => {
+  const maybeCode = unicode[title];
+  return maybeCode ? hexToUnicode(maybeCode) : title;
+};
 
 const hexToUnicode = (hex: string) => String.fromCodePoint(parseInt(hex, 16));
